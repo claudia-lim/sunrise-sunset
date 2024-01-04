11c69464c6b2c5dfbf1d7c9964cf961c02f7975a
Claudia Lim <climweiching@gmail.com>
2024-01-04 15:29:35 +0000

    sunrise sunset api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, React} from 'react'
 import './App.css'
 import InputForm from "./Components/InputForm/index.jsx";
 import Output from "./Components/Output/index.jsx";
+import ToggleButton from "./Components/ToggleButton/index.jsx";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import {faSun, faMoon} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
     const [input, setInput] = useState("");
     const [geoLocJson, setGeoLocJson] = useState({initial:true})
-
+    const [mode, setMode] = useState("day");
+    const [icon, setIcon] = useState("faSun")
+    const [sunrise, setSunrise] = useState("2024-01-04T05:05:35+00:00");
+    const [sunset, setSunset] = useState("2024-01-04T19:22:59+00:00");
     const key = "HQNofba0fy6MwKlkN0KGrlB2Hj88KqTM";
 
     async function fetchGeoLocation () {
@@ -25,15 +32,40 @@ function App() {
         setGeoLocJson(geoLocData);
     }
 
+    useEffect(() => {
+const sunriseTime = new Date(sunrise);
+const sunsetTime = new Date(sunset);
+const current = new Date("2024-01-04T21:22:59+00:00");
+console.log(current);
+console.log(sunriseTime);
+console.log(sunsetTime);
+if (current > sunsetTime || current < sunriseTime) {
+    setMode("night");
+    setIcon("faMoon");
+} else {
+    setMode("day");
+    setIcon("faSun");
+}
+    }, [geoLocJson]);
+
     return (
     <>
-      <div>
-          <h1>Sunrise/Sunset Calculator</h1>
-          <InputForm input={input} setInput={setInput} fetchGeoLocation={fetchGeoLocation}/>
-          <Output geoLocJson={geoLocJson}/>
-      </div>
+        <main className={mode}>
+            <h1>Sunrise/Sunset Calculator</h1>
+            <div className="sun-icon icon">
+                <FontAwesomeIcon icon={faSun} />
+            </div>
+            <div className="moon-icon icon">
+                <FontAwesomeIcon icon={faMoon} />
+            </div>
+
+            <InputForm input={input} setInput={setInput} fetchGeoLocation={fetchGeoLocation}/>
+            <Output geoLocJson={geoLocJson} sunrise={sunrise} sunset={sunset} setSunrise={setSunrise}
+                    setSunset={setSunset}/>
+            <ToggleButton setMode={setMode} mode={mode}/>
+        </main>
     </>
-  )
+    )
 }
 
 export default App
--- src/Components/Output/index.jsx
+++ src/Components/Output/index.jsx
@@ -1,16 +1,19 @@
 import {useEffect, useState} from "react";
 
-function Output ({geoLocJson}) {
+function Output ({geoLocJson, sunrise, sunset, setSunset, setSunrise}) {
 
     const [latitude, setLatitude] = useState(0);
     const [longitude, setLongitude] = useState(0);
     const [city, setCity] = useState("");
+    const [sunriseSunsetData, setSunriseSunsetData] = useState({});
+
 
     useEffect(() => {
         if (!geoLocJson.initial && geoLocJson.results[0].locations[0].adminArea5){
             setLatitude(geoLocJson.results[0].locations[0].latLng.lat);
             setLongitude(geoLocJson.results[0].locations[0].latLng.lng);
             setCity(geoLocJson.results[0].locations[0].adminArea5);
+            // fetchSunriseSunset();
         } else if (geoLocJson.initial){
             setCity("");
         } else {
@@ -21,11 +24,33 @@ function Output ({geoLocJson}) {
 
     }, [geoLocJson]);
 
+
+    async function fetchSunriseSunset () {
+        const customSettings = {
+            method: "GET",
+            headers: {
+                "Origin": "http://localhost:5173"
+            },
+        };
+        const response = await fetch(
+            `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`,
+            customSettings
+        );
+        const sunriseSunsetJson = await response.json();
+        if (sunriseSunsetJson.status === "OK") {
+            setSunriseSunsetData(sunriseSunsetData)
+        } else {
+            console.log('error');
+        }
+    }
+
     return (
         <div>
             <p>{city}</p>
             <p>Latitude = {latitude}</p>
             <p>Longitude = {longitude}</p>
+            <p>Sunrise: {sunrise}</p>
+            <p>Sunset: {sunset}</p>
         </div>
     )
 }
